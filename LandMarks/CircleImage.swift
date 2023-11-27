51d691dd382befafcae6257277ea9b2e03986692
Toy <woohyeon3382@naver.com>
2023-11-28 02:09:55 +0900

    Section4: Create a custom image view

---
+++ LandMarks/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  LandMarks
+//
+//  Created by Toy on 11/28/23.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
