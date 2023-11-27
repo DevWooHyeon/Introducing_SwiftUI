619c03286d66a315c417a8454753b13e9b30afde
Toy <woohyeon3382@naver.com>
2023-11-28 01:44:04 +0900

    Create a new project

---
+++ LandMarks/LandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksApp.swift
+//  LandMarks
+//
+//  Created by Toy on 11/28/23.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
