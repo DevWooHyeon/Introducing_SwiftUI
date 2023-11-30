bf469257c12bd081c47a9d3468ca97f211450697
Toy <woohyeon3382@naver.com>
2023-12-01 01:10:19 +0900

    Section4: Create the list of landmarks

---
+++ LandMarks/Views/LandmarkList.swift
@@ -0,0 +1,21 @@
+//
+//  LandmarkList.swift
+//  LandMarks
+//
+//  Created by Toy on 12/1/23.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
