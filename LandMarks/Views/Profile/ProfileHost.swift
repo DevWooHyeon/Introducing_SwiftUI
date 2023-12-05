6b250025e740e6be3c826eae11510243e1be0e20
Toy <woohyeon3382@naver.com>
2023-12-05 19:17:29 +0900

    Section1: Display a user profile

---
+++ LandMarks/Views/Profile/ProfileHost.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileHost.swift
+//  LandMarks
+//
+//  Created by toy on 12/5/23.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ProfileHost()
+}
