fd6f7fd0ec096d4de688e8f8c35588155a4df0b8
Toy <woohyeon3382@naver.com>
2023-12-05 16:45:32 +0900

    Section3: Draw the badge symbol

---
+++ LandMarks/RotatedBadgeSymbol.swift
@@ -0,0 +1,22 @@
+//
+//  RotatedBadgeSymbol.swift
+//  LandMarks
+//
+//  Created by 김우현 on 12/5/23.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 5))
+}
