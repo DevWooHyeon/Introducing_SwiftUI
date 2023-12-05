740b017aee45ab72c70ec569697bdbc3f16532a1
Toy <woohyeon3382@naver.com>
2023-12-05 18:19:19 +0900

    Section1: Add a category view

---
+++ LandMarks/CategoryHome.swift
@@ -0,0 +1,25 @@
+//
+//  CategoryHome.swift
+//  LandMarks
+//
+//  Created by Toy on 12/5/23.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationSplitView {
+            Text("Hello, World!")
+                .navigationTitle("Featured")
+        } detail: {
+            Text("Select a Landmark")
+        }
+
+        
+    }
+}
+
+#Preview {
+    CategoryHome()
+}
